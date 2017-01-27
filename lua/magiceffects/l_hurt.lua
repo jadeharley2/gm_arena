f33cf8a231da3fcc7eb16e3aafdb97e7a2232f5e
jadeharley2 <tim29022@gmail.com>
2017-01-27 03:04:57 +0300

    char update

--- lua/magiceffects/l_hurt.lua
+++ lua/magiceffects/l_hurt.lua
@@ -1,11 +1,17 @@
 AddCSLuaFile()
 effect.amount = 10
+effect.dmg_type = DMG_SLASH
 function effect:Begin(ent) 
 	if (ent:IsPlayer() or ent:IsNPC())  then
 		local admg = 1
 		if self.owner then admg =  self.owner.stats.attackdamage end
 		//local resist = ent.stats.
-		ent:TakeDamage(admg,self.owner,NULL)
+		local d = DamageInfo()
+		d:SetDamage( ent:Health() )
+		d:SetAttacker( self.owner )
+		d:SetDamageType( self.dmg_type ) 
+		ent:TakeDamageInfo( d )
+		//ent:TakeDamage(admg,self.owner,NULL)
 		return true
 	end 
 end     
